dd84672855b45b2b5b03c6fc5a1dc1abc7e58713
Binkitubo <davidlolleria@hotmail.com>
2022-08-25 16:12:54 +0000

    Initial commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,17 +3,42 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+		<div className="container">
+			<input type="checkbox" id="toggle" />
+			<div className="signin">
+				<h1 className="fw-bold">Sign In</h1>
+				<form>
+					<div className="social">
+						<a href="#"><i className="fab fa-google-plus-g"></i></a>
+						<a href="#"><i className="fab fa-facebook-f"></i></a>
+						<a href="#"><i className="fab fa-linkedin-in"></i></a>
+					</div>
+					<p>Or enter your info</p>
+					<input type="email" placeholder="Email" />
+					<input type="password" placeholder="Password" />
+					<small>Forgot Password?</small>
+					<input type="submit" value="Login" />
+					<small>Don't have an account? <label for="toggle">Sign Up</label></small>
+				</form>
 			</div>
-		</nav>
+			
+			<div className="signup">
+				<h1 className="fw-bold">Create an account</h1>
+				<form>
+					<div className="social">
+						<a href="#"><i className="fab fa-google-plus-g"></i></a>
+						<a href="#"><i className="fab fa-facebook-f"></i></a>
+						<a href="#"><i className="fab fa-linkedin-in"></i></a>
+					</div>
+					<p>Or enter your info</p>
+					<input type="text" placeholder="Name" />
+					<input type="email" placeholder="Email" />
+					<input type="password" placeholder="Password" />
+					<input type="password" placeholder="Confirm Password" />
+					<input type="submit" value="Create Account" />
+					<small>Already have an account? <label for="toggle">Sign In</label></small>
+				</form>
+			</div>
+		</div>
 	);
 };
